70b8991d890703cbda3d6746497c814c9748dd81
akash <akashagarwal0403@gmail.com>
2019-06-08 01:44:50 +0530

    serializer done

--- src/tweets/api/serializers.py
+++ src/tweets/api/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 from accounts.api.serializers import UserModalSerializers
 from tweets.models import Tweet
+from django.utils.timesince import timesince
 
 class TweetModalSerializers(serializers.ModelSerializer):
-    user = UserModalSerializers()
+    user = UserModalSerializers(read_only = True)
+    timesince = serializers.SerializerMethodField()
+    date_display = serializers.SerializerMethodField()
+
     class Meta:
         model = Tweet
-        fields = ['user' , 'content']
+        fields = [
+            'user' , 
+            'content',
+            'timestamp',
+            'date_display',
+            'timesince'
+            ]
+    
+    def get_date_display(self , obj):
+        return obj.timestamp.strftime("%b %d, %I:%M %p")
+
+    def get_timesince(self,obj):
+        return timesince(obj.timestamp) + " ago"
--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,12 +1,27 @@
 from rest_framework import generics
 from .serializers import TweetModalSerializers
 from tweets.models import Tweet
+from django.db.models import Q
+from rest_framework import permissions
 
+class TweetCreateAPIView(generics.CreateAPIView):
+    serializer_class = TweetModalSerializers
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModalSerializers
 
     def get_queryset(self,*args,**kwargs):
-        return Tweet.objects.all() 
+        qs = Tweet.objects.all().order_by("-timestamp")
+        query = self.request.GET.get('q',None)
+        if query is not None:
+            qs = qs.filter(
+                Q(content__icontains=query) | 
+                Q(user__username__icontains=query)
+            )
+        return qs
 
 
