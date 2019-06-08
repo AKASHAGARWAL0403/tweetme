5f0d6510e5cf1db75135bb0da3275c76d9774d07
akash <akashagarwal0403@gmail.com>
2019-06-08 07:58:48 +0530

    Page panigation added

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -3,6 +3,7 @@ from .serializers import TweetModalSerializers
 from tweets.models import Tweet
 from django.db.models import Q
 from rest_framework import permissions
+from .pagination import StandardResultPagination 
 
 class TweetCreateAPIView(generics.CreateAPIView):
     serializer_class = TweetModalSerializers
@@ -13,7 +14,8 @@ class TweetCreateAPIView(generics.CreateAPIView):
 
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModalSerializers
-
+    pagination_class = StandardResultPagination
+    
     def get_queryset(self,*args,**kwargs):
         qs = Tweet.objects.all().order_by("-timestamp")
         query = self.request.GET.get('q',None)
